7d4fc81476bf214c98966ff92357a6f6c416b30c
filipposchr <philipp960@gmail.com>
2025-06-30 14:26:47 +0200

    Update test_multiple.py

--- test_multiple.py
+++ test_multiple.py
@@ -1,12 +1,19 @@
+import argparse
 import subprocess
-datasets = ["edit-mathoverflow", "edit-facebook_wall", "edit-topology", "edit-mlwikiquote", "edit-plwikiquote", "edit-digg_reply", "edit-SMS", "edit-rt-pol", "edit-slashdot_reply","edit-wamazon", "edit-mgwikipedia", "edit-tgwiktionary", "edit-ltwiktionary"]
+
+datasets = [ "edit-mathoverflow", "edit-facebook_wall", "edit-topology",  "edit-mlwikiquote", "edit-plwikiquote", "edit-digg_reply", "edit-SMS", "edit-rt-pol", "edit-slashdot_reply","edit-wamazon", "edit-mgwikipedia", "edit-tgwiktionary", "edit-ltwiktionary"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--bet", choices=["sfm", "sh"], required=True, help="Choose between 'sfm' or 'sh'")
+args = parser.parse_args()
 
 # Loop over each dataset
 for d in datasets:
     print(f"\n - Running evaluation for dataset: *{d}* -")
 
     # Construct command
-    command = ["python", "-u", "main.py", "-d", d, "--bet", "sfm", "--test"]
+    command = ["python", "-u", "main.py", "-d", d, "--bet", args.bet, "--test"]
+
     # Run the command
     result = subprocess.run(command)
 
